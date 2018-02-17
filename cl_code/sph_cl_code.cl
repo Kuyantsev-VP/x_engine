d4291a7dc83435f6a78db2c40f3e46c1c07acc3d
Kuyantsev-VP <vkujantsev94@gmail.com>
2018-02-17 20:35:01 +0700

    for debug

--- cl_code/sph_cl_code.cl
+++ cl_code/sph_cl_code.cl
@@ -31,181 +31,306 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
  * USE OR OTHER DEALINGS IN THE SOFTWARE.
  *******************************************************************************/
- 
+
 #ifdef cl_amd_printf
-	#pragma OPENCL EXTENSION cl_amd_printf : enable
-	#define PRINTF_ON // this comment because using printf leads to very slow work on Radeon r9 290x on my machine
-    	              // don't know why
+#pragma OPENCL EXTENSION cl_amd_printf : enable
+#define PRINTF_ON // this comment because using printf leads to very slow work
+                  // on Radeon r9 290x on my machine
+                  // don't know why
 #elif defined(cl_intel_printf)
-	#pragma OPENCL EXTENSION cl_intel_printf : enable
-	#define PRINTF_ON
+#pragma OPENCL EXTENSION cl_intel_printf : enable
+#define PRINTF_ON
 #endif
 
 #ifdef _DOUBLE_PRECISION
 #ifdef cl_khr_fp64
-	#pragma OPENCL EXTENSION cl_khr_fp64 : enable
-	
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+
 #elif defined(cl_amd_fp64)
-	#pragma OPENCL EXTENSION cl_amd_fp64 : enable
-	#define _DOUBLE_PRECISION
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#define _DOUBLE_PRECISION
 #else
-	#error "Double precision floating point not supported by OpenCL implementation."
+#error "Double precision floating point not supported by OpenCL implementation."
 #endif
 #endif
-#if defined(_WIN32) || defined(_WIN64)
+
 #include "inc\\ocl_struct.h"
-#else
-#include "inc/ocl_struct.h"
-#endif
 
-typedef struct particle_f{
-	float4 pos;
-	float4 vel;
-	size_t cell_id;
-	size_t type_;
-	float density;
-	float pressure;
+#define NO_PARTICLE_ID -1
+
+typedef struct particle_f {
+  float4 pos;
+  float4 vel;
+  size_t cell_id;
+  size_t type_;
+  float density;
+  float pressure;
 } particle_f;
-
 #ifdef _DOUBLE_PRECISION
-typedef struct particle_d{
-	double4 pos;
-	double4 vel;
-	size_t cell_id;
-	size_t type_;
-	double density;
-	double pressure;
+typedef struct particle_d {
+  double4 pos;
+  double4 vel;
+  size_t cell_id;
+  size_t type_;
+  double density;
+  double pressure;
 } particle_d;
 #endif
 
-
 /** Just for test
-*/
-__kernel void work_with_struct(__global struct extendet_particle * ext_particles, 
-							   __global struct 
-							   #ifdef _DOUBLE_PRECISION
-									particle_d
-							   #else
-									particle_f
-							   #endif 
-									* particles){
-	int id = get_global_id(0);
+ */
+__kernel void work_with_struct(__global struct extendet_particle *ext_particles,
+                               __global struct
+#ifdef _DOUBLE_PRECISION
+                               particle_d
+#else
+                               particle_f
+#endif
+                                   *particles) {
+  int id = get_global_id(0);
 #ifdef PRINTF_ON
-	if(id == 0){
-		printf("sizeof() of particles_f is %d\n", sizeof(particle_f) );
-		#ifdef _DOUBLE_PRECISION
-		printf("sizeof() of particles_d is %d\n", sizeof(particle_d) );
-		#endif
-	}
+  if (id == 0) {
+    printf("sizeof() of particles_f is %d\n", sizeof(particle_f));
+#ifdef _DOUBLE_PRECISION
+    printf("sizeof() of particles_d is %d\n", sizeof(particle_d));
+#endif
+  }
 #endif
 #ifdef _DOUBLE_PRECISION
 
-	particles[id].pos = (double4)(id, id, id, id);
-	particles[id].vel = (double4)(id, id, id, id);
+  particles[id].pos = (double4)(id, id, id, id);
+  particles[id].vel = (double4)(id, id, id, id);
 #else
-	particles[id].pos = (float4)(id, id, id, id);
-	particles[id].vel = (float4)(id, id, id, id);
+  particles[id].pos = (float4)(id, id, id, id);
+  particles[id].vel = (float4)(id, id, id, id);
 #endif
-	particles[id].type_ = id + 1;
+  particles[id].type_ = id + 1;
 }
 
-/**Initialization of neighbour list by -1 
-* what means that it's no neighbours. 
-*/
-__kernel void _ker_init_ext_particles(__global struct extendet_particle * ext_particles){
-	int id = get_global_id(0);
-	ext_particles[id].p_id = id;
-	for(int i=0;i<NEIGHBOUR_COUNT;++i){
-		ext_particles[id].neigbour_list[i] = -1;
-	}
+__kernel void _ker_grid_cell_indexing(__global struct
+#ifdef _DOUBLE_PRECISION
+                                      particle_d
+#else
+                                      particle_f
+#endif
+                                          *particles,
+                                      uint PARTICLE_COUNT,
+                                      __global uint *grid_cell_index,
+                                      uint grid_cell_count) {
+  int cell_id_to_find = get_global_id(0);
+
+  if (cell_id_to_find == 1) {
+  printf("\n\t sizeof(particle_f) = %d; \n", sizeof(particle_f));
+    printf("\n\t I'm in kernel! PARTICLE_COUNT = %d; grid_cell_count = %d; "
+           "cell_id_to_find = %d \n",
+           PARTICLE_COUNT, grid_cell_count, cell_id_to_find);
+    printf("\n\t particles[%d].cell_id = %d; \n", cell_id_to_find,
+           particles[cell_id_to_find].cell_id);
+  }
+  // check for trivial cases
+  if (cell_id_to_find > grid_cell_count) {
+    return;
+  }
+
+  if (cell_id_to_find == 1) {
+    printf("\n\t Incorrect cases checked! \n");
+  }
+
+  if (PARTICLE_COUNT == 0) {
+    /* array is empty */
+    return;
+  } else if (particles[0].cell_id > cell_id_to_find) {
+
+    if (cell_id_to_find == 1) {
+      printf("\n\t OOPS! particles[%d].cell_id = %d; \n", cell_id_to_find,
+           particles[cell_id_to_find].cell_id);
+    }
+
+    /* cell_id_to_find is lower than everyone else */
+    return;
+  } else if (particles[PARTICLE_COUNT - 1].cell_id < cell_id_to_find) {
+    /* cell_id_to_find is greater than everyone else */
+    return;
+  }
+
+  if (cell_id_to_find == 0) {
+    grid_cell_index[cell_id_to_find] = 0;
+    return;
+  }
+  if (cell_id_to_find == grid_cell_count) {
+    grid_cell_index[cell_id_to_find] = PARTICLE_COUNT;
+    return;
+  }
+  // end check
+
+  if (cell_id_to_find == 1) {
+    printf("\n\t All trivial checks passed! \n");
+  }
+
+  // binary search
+  int low = 0;
+  int high = PARTICLE_COUNT - 1;
+  bool converged = false;
+  bool found = false;
+  int particle_id = NO_PARTICLE_ID;
+  int cur_part_cell_id = -1;
+
+  while (!converged && !found) {
+    if (low > high) {
+      converged = true;
+      particle_id = NO_PARTICLE_ID;
+      continue;
+    }
+
+    if (low == high) {
+      found = true;
+      particle_id = low;
+      continue;
+    }
+
+    int middle = low + (high - low) / 2;
+    cur_part_cell_id = particles[middle].cell_id;
+    particle_id = middle;
+    if (cell_id_to_find <= cur_part_cell_id) {
+      high = middle;
+    } else {
+      low = middle + 1;
+    }
+  }
+  // end binary search
+
+  grid_cell_index[cell_id_to_find] = particle_id;
+  printf("cell_to_find = %d; grid_cell_index[%d] = %d\n", cell_id_to_find,
+         cell_id_to_find, grid_cell_index[cell_id_to_find]);
+}
+
+/**Initialization of neighbour list by -1
+ * what means that it's no neighbours.
+ */
+__kernel void
+_ker_init_ext_particles(__global struct extendet_particle *ext_particles) {
+  int id = get_global_id(0);
+  ext_particles[id].p_id = id;
+  for (int i = 0; i < NEIGHBOUR_COUNT; ++i) {
+    ext_particles[id].neigbour_list[i] = -1;
+  }
 }
 
 /** Calc current cell id for each particles
-*/
-__kernel void _ker_calc_cell_id(__global struct 
-							   #ifdef _DOUBLE_PRECISION
-									particle_d
-							   #else
-									particle_f
-							   #endif 
-									* particles){
-
+ */
+__kernel void _ker_calc_cell_id(__global struct
+#ifdef _DOUBLE_PRECISION
+                                particle_d
+#else
+                                particle_f
+#endif
+                                    *particles) {
 }
 
 /** Searchin for neigbours foe each particles
-*/
-__kernel void _ker_neighbour_search(__global struct extendet_particle * ext_particles, 
-							   __global struct 
-							   #ifdef _DOUBLE_PRECISION
-									particle_d
-							   #else
-									particle_f
-							   #endif 
-									* particles){
-
+ */
+__kernel void
+_ker_neighbour_search(__global struct extendet_particle *ext_particles,
+                      __global struct
+#ifdef _DOUBLE_PRECISION
+                      particle_d
+#else
+                      particle_f
+#endif
+                          *particles) {
 }
 
-int cell_id(
-		   int4 cell_factors_,
-		   uint grid_cells_X,
-		   uint grid_cells_Y,
-		   uint grid_cells_Z//don't use
-		   )
-{
-	int cell_id = cell_factors_.x + cell_factors_.y * grid_cells_X
-		+ cell_factors_.z * grid_cells_X * grid_cells_Y;
-	return cell_id;
+int cellId(int4 cellFactors_, uint gridCellsX, uint gridCellsY,
+           uint gridCellsZ // don't use
+) {
+  int cellId_ = cellFactors_.x + cellFactors_.y * gridCellsX +
+                cellFactors_.z * gridCellsX * gridCellsY;
+  return cellId_;
 }
 /** Caculation spatial hash cellId for every particle
  *  Kernel fill up particleIndex buffer.
  */
- int4 cell_factors(
-				 __global struct 
-				#ifdef _DOUBLE_PRECISION
-					particle_d
-				#else
-					particle_f
-				#endif 
-					* particle,
-				 float x_min,
-				 float y_min,
-				 float z_min,
-				 float hash_grid_cell_size_inv
-				 )
-{
-	//xmin, ymin, zmin
-	int4 result;
-	result.x = (int)( particle->pos.x *  hash_grid_cell_size_inv );
-	result.y = (int)( particle->pos.y *  hash_grid_cell_size_inv );
-	result.z = (int)( particle->pos.z *  hash_grid_cell_size_inv );
-	return result;
+int4 cellFactors(__global struct
+#ifdef _DOUBLE_PRECISION
+                 particle_d
+#else
+                 particle_f
+#endif
+                     *particle,
+                 float xmin, float ymin, float zmin,
+                 float hashGridCellSizeInv) {
+  // xmin, ymin, zmin
+  int4 result;
+  result.x = (int)(particle->pos.x * hashGridCellSizeInv);
+  result.y = (int)(particle->pos.y * hashGridCellSizeInv);
+  result.z = (int)(particle->pos.z * hashGridCellSizeInv);
+  return result;
 }
-__kernel void hashParticles(
-							__global struct 
-							#ifdef _DOUBLE_PRECISION
-								particle_d
-							#else
-								particle_f
-							#endif 
-								* particles,
-							uint gridCellsX,
-							uint gridCellsY,
-							uint gridCellsZ,
-							float hashGridCellSizeInv,
-							float xmin,
-							float ymin,
-							float zmin,
-							__global uint2 * particleIndex,
-							uint   PARTICLE_COUNT
-							)
-{
-	/*int id = get_global_id( 0 );
-	if( id >= PARTICLE_COUNT ) return;
-	float4 _position = position[ id ];
-	int4 cellFactors_ = cellFactors( _position, xmin, ymin, zmin, hashGridCellSizeInv );
-	int cellId_ = cellId( cellFactors_, gridCellsX, gridCellsY, gridCellsZ ) & 0xffffff; // truncate to low 16 bits
-	uint2 result;
-	PI_CELL_ID( result ) = cellId_;
-	PI_SERIAL_ID( result ) = id;
-	particleIndex[ id ] = result;*/
+__kernel void hashParticles(__global struct
+#ifdef _DOUBLE_PRECISION
+                            particle_d
+#else
+                            particle_f
+#endif
+                                *particles,
+                            uint gridCellsX, uint gridCellsY, uint gridCellsZ,
+                            float hashGridCellSizeInv, float xmin, float ymin,
+                            float zmin, __global uint2 *particleIndex,
+                            uint PARTICLE_COUNT) {
+  /*int id = get_global_id( 0 );
+  if( id >= PARTICLE_COUNT ) return;
+  float4 _position = position[ id ];
+  int4 cellFactors_ = cellFactors( _position, xmin, ymin, zmin,
+  hashGridCellSizeInv ); int cellId_ = cellId( cellFactors_, gridCellsX,
+  gridCellsY, gridCellsZ ) & 0xffffff; // truncate to low 16 bits uint2 result;
+  PI_CELL_ID( result ) = cellId_;
+  PI_SERIAL_ID( result ) = id;
+  particleIndex[ id ] = result;*/
 }
+
+//__kernel void benchmarking() {
+//	return;
+//}
+//
+//
+// void matrixMultiplication(__global float* A, __global float* B, __global
+// float* C, int widthA, int widthB) { 	int i = get_global_id(0); 	int j =
+// get_global_id(1); 	float value = 0; 	for (int k = 0; k < widthA; k++)
+//	{
+//		value = value + A[k + j * widthA] * B[k*widthB + i];
+//	}
+//	C[i + widthA * j] = value;
+//}
+
+// typedef struct {
+// 	int width;
+// 	int height;
+// 	__global float* elements;
+// } Matrix;
+// // Thread block size
+// #define BLOCK_SIZE 16
+// // Matrix multiplication function called by MatMulKernel()
+// void matrixMul(Matrix A, Matrix B, Matrix C)
+// {
+// 	float Cvalue = 0;
+// 	int row = get_global_id(1);
+// 	int col = get_global_id(0);
+// 	for (int e = 0; e < A.width; ++e)
+// 		Cvalue += A.elements[row * A.width + e]
+// 		* B.elements[e * B.width + col];
+// 	C.elements[row * C.width + col] = Cvalue;
+// }
+// // Matrix multiplication kernel called by MatMulHost()
+// __kernel void MatMulKernel(
+// 	int Awidth, int Aheight, __global float* Aelements,
+// 	int Bwidth, int Bheight, __global float* Belements,
+// 	int Cwidth, int Cheight, __global float* Celements,
+// 	int factor)
+// {
+// 	Matrix A = { Awidth, Aheight, Aelements };
+// 	Matrix B = { Bwidth, Bheight, Belements };
+// 	Matrix C = { Cwidth, Cheight, Celements };
+// 	for (int i = 0; i < factor; ++i) {
+// 		matrixMul(A, B, C);
+// 	}
+// }
--- inc/ocl_solver.hpp
+++ inc/ocl_solver.hpp
@@ -42,6 +42,8 @@
 #else
 #include <CL/cl.hpp>
 #endif
+
+#include "../X_ENGINE/measurement.hpp"
 #include "isolver.h"
 #include "ocl_const.h"
 #include "ocl_struct.h"
@@ -51,15 +53,27 @@
 #include "x_device.h"
 #include <fstream>
 #include <iostream>
+
 namespace x_engine {
 namespace solver {
+
+typedef struct {
+  int width;
+  int height;
+  float *elements;
+} Matrix;
+
+void matrix_multiply(Matrix A, Matrix B, Matrix C, const cl::Context context,
+                     cl::Kernel matMulKernel, const cl::CommandQueue queue,
+                     uint32_t factor);
+
 using std::cout;
 using std::endl;
 using std::shared_ptr;
+using x_engine::ocl_error;
 using x_engine::model::particle;
 using x_engine::model::partition;
 using x_engine::model::sph_model;
-using x_engine::ocl_error;
 // OCL constans block
 #define QUEUE_EACH_KERNEL 1
 
@@ -67,6 +81,12 @@ const int LOCAL_NDRANGE_SIZE = 256;
 
 template <class T = float> class ocl_solver : public i_solver {
   typedef shared_ptr<sph_model<T>> model_ptr;
+
+private:
+  float get_random_value(float min, float max) {
+    return min + static_cast<float>(rand()) /
+                     (static_cast<float>(RAND_MAX / (max - min)));
+  }
 
 public:
   ocl_solver(model_ptr &m, shared_ptr<device> d) : model(m), dev(d) {
@@ -76,35 +96,94 @@ public:
       throw;
     }
   }
-  // TODO rename method!!!
   virtual void init_model(const partition &p) {
     this->p = p;
     init_buffers();
     init_kernels();
   }
+
   ~ocl_solver() {}
-  virtual void run_neighbour_search() {}
+  virtual void run_neighbour_search() { run_grid_cell_indexing(); }
   virtual void run_physic() {}
+
+  void benchmark_solver() {
+    srand(time(NULL));
+    Matrix A;
+    Matrix B;
+    Matrix C;
+    const float min_val = 0.0;
+    const float max_val = 500.0;
+    const int height = 2048;
+    const int width = 2048;
+
+    A.elements = (float *)malloc(height * width * sizeof(float));
+    for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        A.elements[i * height + j] = get_random_value(min_val, max_val);
+      }
+    }
+    // A.elements = elements;
+    A.height = height;
+    A.width = width;
+
+    B.elements = (float *)malloc(height * width * sizeof(float));
+    for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        B.elements[i * height + j] = get_random_value(min_val, max_val);
+      }
+    }
+    // B.elements = elements;
+    B.height = height;
+    B.width = width;
+
+    C.elements = (float *)malloc(height * width * sizeof(float));
+    for (int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        C.elements[i * height + j] = 0;
+      }
+    }
+    // C.elements = elements;
+    C.height = height;
+    C.width = width;
+
+    cl::Kernel mat_mul_kernel;
+    create_ocl_kernel("MatMulKernel", mat_mul_kernel);
+    matrix_multiply(A, B, C, dev->context, mat_mul_kernel, queue, 1);
+  }
 
 private:
   model_ptr model;
   partition p;
+  // TODO Think about moving this feild into the partition struct
+  int partition_cell_count;
   shared_ptr<device> dev;
   std::string msg = dev->name + '\n';
-  const std::string cl_program_file = "cl_code//sph_cl_code.cl";
+  const std::string cl_program_file = "cl_code\\sph_cl_code.cl";
   cl::Kernel k_init_ext_particles;
+  cl::Kernel k_grid_cell_index;
   cl::Buffer b_particles;
   cl::Buffer b_ext_particles;
+  cl::Buffer b_grid_cell_index;
   cl::CommandQueue queue;
   cl::Program program;
+
+  // Initialization device-side, setting size and naming
   void init_buffers() {
     create_ocl_buffer("particles", b_particles, CL_MEM_READ_WRITE,
                       p.size() * sizeof(particle<T>));
     create_ocl_buffer("ext_particles", b_ext_particles, CL_MEM_READ_WRITE,
                       p.size() * sizeof(extendet_particle));
+
+    copy_buffer_to_device((void*)&(model->get_particles()[p.start]), b_particles,
+                          p.size() * sizeof(particle<T>));
   }
-  void init_kernels() {}
+
+  void init_kernels() {
+    create_ocl_kernel("_ker_grid_cell_indexing", k_grid_cell_index);
+  }
+
   virtual void init_ext_particles() {}
+
   void initialize_ocl() {
     int err;
     queue = cl::CommandQueue(dev->context, dev->dev, 0, &err);
@@ -113,8 +192,9 @@ private:
     }
     std::ifstream file(cl_program_file);
     if (!file.is_open()) {
-      throw ocl_error(msg + "Could not open file with OpenCL program check "
-                            "input arguments oclsourcepath: " +
+      throw ocl_error(msg +
+                      "Could not open file with OpenCL program check "
+                      "input arguments oclsourcepath: " +
                       cl_program_file);
     }
     std::string programSource(std::istreambuf_iterator<char>(file),
@@ -133,7 +213,7 @@ private:
 #if INTEL_OPENCL_DEBUG
     err = program.build(OPENCL_DEBUG_PROGRAM_PATH + "-g -cl-opt-disable -I .");
 #else
-    err = program.build("-I .");
+    err = program.build("");
 #endif
 #endif
     if (err != CL_SUCCESS) {
@@ -148,6 +228,53 @@ private:
         << cl_program_file << std::endl;
     return;
   }
+
+  void matrix_multiply(Matrix A, Matrix B, Matrix C, const cl::Context context,
+                       cl::Kernel matMulKernel, const cl::CommandQueue queue,
+                       uint32_t factor) {
+    const int BLOCK_SIZE = 16;
+    // Load A and B to device memory
+    size_t size = A.width * A.height * sizeof(float);
+    cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, size);
+    queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, size, A.elements);
+    size = B.width * B.height * sizeof(float);
+    cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, size);
+    queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, size, B.elements);
+    // Allocate C in device memory
+    size = C.width * C.height * sizeof(float);
+    cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, size);
+    queue.enqueueWriteBuffer(buffer_C, CL_TRUE, 0, size, C.elements);
+
+    cl::Buffer resBuf(context, CL_MEM_READ_WRITE, sizeof(int));
+    // Invoke kernel
+    int i = 0;
+
+    matMulKernel.setArg(i++, A.width);
+    matMulKernel.setArg(i++, A.height);
+    matMulKernel.setArg(i++, buffer_A);
+    matMulKernel.setArg(i++, B.width);
+    matMulKernel.setArg(i++, B.height);
+    matMulKernel.setArg(i++, buffer_B);
+    matMulKernel.setArg(i++, C.width);
+    matMulKernel.setArg(i++, C.height);
+    matMulKernel.setArg(i++, buffer_C);
+    matMulKernel.setArg(i++, factor);
+
+    cl::NDRange global(C.height, C.width);
+    queue.enqueueNDRangeKernel(matMulKernel, cl::NullRange, global,
+                               cl::NullRange);
+    refreshTime();
+    queue.finish();
+    std::string naming("");
+    naming += (*dev).name + " becnh results: \t%9.3f ms\n";
+    watch_report(naming.c_str());
+
+    float *res_1 = (float *)malloc(C.height * C.width * sizeof(float));
+    ;
+    queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, size, res_1);
+    // queue.enqueueReadBuffer(resBuf, 1, 0, C.height * C.width, res_1);
+  }
+
   void create_ocl_buffer(const char *name, cl::Buffer &b,
                          const cl_mem_flags flags, const int size) {
     int err;
@@ -158,6 +285,7 @@ private:
       throw ocl_error(error_m);
     }
   }
+
   void create_ocl_kernel(const char *name, cl::Kernel &k) {
     int err;
     k = cl::Kernel(program, name, &err);
@@ -166,6 +294,194 @@ private:
       error_m.append(name);
       throw ocl_error(error_m);
     }
+  }
+
+  void copy_buffer_to_device(const void *host_b, cl::Buffer &ocl_b,
+                             const int size) {
+    // Actualy we should check  size and type
+    int err = queue.enqueueWriteBuffer(ocl_b, CL_TRUE, 0, size, host_b);
+    if (err != CL_SUCCESS) {
+      std::string error_m =
+          "Could not enqueue read data from buffer  error code is: ";
+      error_m.append(std::to_string(err));
+      throw ocl_error(error_m);
+    }
+    queue.finish();
+  }
+
+  void copy_buffer_from_device(void *host_b, const cl::Buffer &ocl_b,
+                               const int size) {
+    // Actualy we should check  size and type
+    int err = queue.enqueueReadBuffer(ocl_b, CL_TRUE, 0, size, host_b);
+    if (err != CL_SUCCESS) {
+      std::string error_m =
+          "Could not enqueue read data from buffer  error code is: ";
+      error_m.append(std::to_string(err));
+      throw ocl_error(error_m);
+    }
+    queue.finish();
+  }
+
+  void single_thread_grid_cell_indexing(std::vector<particle<T>> particles, int PARTICLE_COUNT, std::vector<int>& grid_cell_index, int grid_cell_count, int cell_id_to_find) {
+	  const int NO_PARTICLE_ID = -1;
+
+	  //check for trivial cases
+	  if (cell_id_to_find > grid_cell_count) {
+		  return;
+	  }
+	  if (PARTICLE_COUNT == 0) {
+		  /* array is empty */
+		  return;
+	  }
+	  else if (particles[0].cell_id > cell_id_to_find) {
+		  /* cell_id_to_find is lower than everyone else */
+		  return;
+	  }
+	  else if (particles[PARTICLE_COUNT - 1].cell_id < cell_id_to_find) {
+		  /* cell_id_to_find is greater than everyone else */
+		  return;
+	  }
+
+	  if (cell_id_to_find == 0) {
+		  grid_cell_index[cell_id_to_find] = 0;
+		  return;
+	  }
+	  if (cell_id_to_find == grid_cell_count) {
+		  grid_cell_index[cell_id_to_find] = PARTICLE_COUNT;
+		  return;
+	  }
+	  //end check
+
+	  //binary search
+	  int low = 0;
+	  int high = PARTICLE_COUNT - 1;
+	  bool converged = false;
+	  bool found = false;
+	  int particle_id = NO_PARTICLE_ID;
+	  int cur_part_cell_id = -1;
+
+	  while (!converged && !found) {
+		  if (low > high) {
+			  converged = true;
+			  particle_id = NO_PARTICLE_ID;
+			  continue;
+		  }
+
+		  if (low == high) {
+			  found = true;
+			  particle_id = low;
+			  continue;
+		  }
+
+		  int middle = low + (high - low) / 2;
+		  cur_part_cell_id = particles[middle].cell_id;
+		  particle_id = middle;
+		  if (cell_id_to_find <= cur_part_cell_id) {
+			  high = middle;
+		  }
+		  else {
+			  low = middle + 1;
+		  }
+	  }
+	  //end binary search
+
+	  //find lowest particle_id that gives cell_id_to_find and init grid_cell_index value
+	  if (!converged && found && particles[particle_id].cell_id == cell_id_to_find) {
+		  particle_id = low;
+		  //[QUEST] Is there no need to check if high == 0 ? I think so because if high == 0 it means that we wanted to find cell_id_to_find == 0. That case was handled in trivial cases
+
+		  //find lowest particle_id that gives cell_id_to_find
+		  // [QUEST] is the while condition below correct?
+		  while ((particle_id > 0) && (particles[particle_id - 1].cell_id == cell_id_to_find)) {
+			  particle_id--;
+		  }
+	  }
+	  else {
+		  particle_id = NO_PARTICLE_ID;
+	  }
+	  grid_cell_index[cell_id_to_find] = particle_id;
+	  std::cout << "cell_to_find = " << cell_id_to_find << "; grid_cell_index[" << cell_id_to_find << "] = " << grid_cell_index[cell_id_to_find] << std::endl;
+  }
+
+
+  unsigned int run_grid_cell_indexing() {
+    particle<T> particle_start = model->get_particles().at(p.start);
+    std::cout << "p_start cell id: " << particle_start.cell_id << std::endl;
+    particle<T> particle_end = model->get_particles().at(p.end - 1);
+    std::cout << "p_end cell id: " << particle_end.cell_id << std::endl;
+
+    int grid_cell_index_size =
+        particle_end.cell_id - particle_start.cell_id + 1;
+    int grid_cell_index_size_rounded_up =
+        ((grid_cell_index_size - 1) / LOCAL_NDRANGE_SIZE + 1) *
+        LOCAL_NDRANGE_SIZE;
+
+    create_ocl_buffer("grid_cell_index", b_grid_cell_index, CL_MEM_READ_WRITE,
+                      grid_cell_index_size * sizeof(int));
+
+    std::vector<int> grid_cell_index(grid_cell_index_size);
+
+    std::cout << "vector size: " << grid_cell_index.size() << std::endl;
+
+	std::vector<particle<T>> solver_particles = model->get_particles();
+	solver_particles.erase(solver_particles.begin(), solver_particles.begin() + p.start);
+	solver_particles.erase(solver_particles.begin() + p.end, solver_particles.end());
+
+	std::cout << "particles to calc:" << std::endl;
+	for (int j = 0; j < solver_particles.size(); j++) {
+		std::cout << j << ':' << solver_particles[j].cell_id << "| ";
+	}
+	std::cout << std::endl;
+
+	//for (int cell_id_to_find = 0; cell_id_to_find < grid_cell_index.size(); cell_id_to_find++) {
+	//	single_thread_grid_cell_indexing(solver_particles, solver_particles.size(), grid_cell_index, grid_cell_index.size(), cell_id_to_find);
+	//}
+	
+	std::cout << "grid_cell_index:" << std::endl;
+	for (int j : grid_cell_index) {
+		std::cout << j << ' ';
+	}
+	std::cout << std::endl;
+
+    //(auto j: grid_cell_index)
+    // copy_buffer_to_device(&(grid_cell_index.at(0)), b_grid_cell_index,
+    //                      grid_cell_index_size * sizeof(int));
+    copy_buffer_to_device((void *)(&grid_cell_index[0]), b_grid_cell_index,
+                          grid_cell_index_size * sizeof(int));
+
+	std::cout << "sizeof(particle) = " << sizeof(particle<T>) << std::endl;
+
+    int i = 0;
+    k_grid_cell_index.setArg(i++, b_particles);
+    k_grid_cell_index.setArg(i++, p.size());
+    k_grid_cell_index.setArg(i++, b_grid_cell_index);
+    k_grid_cell_index.setArg(i++, grid_cell_index_size);
+
+    int err =
+        queue.enqueueNDRangeKernel(k_grid_cell_index, cl::NullRange,
+                                   cl::NDRange(grid_cell_index_size_rounded_up),
+                                   cl::NDRange(LOCAL_NDRANGE_SIZE), NULL, NULL);
+    //std::cout << "grid_cell_index kernel is invoked" << std::endl;
+	queue.flush();
+    queue.finish();
+    std::cout << "grid_cell_index kernel is finished" << std::endl;
+
+    copy_buffer_from_device((void *)(&grid_cell_index[0]), b_grid_cell_index,
+                            grid_cell_index.size() * sizeof(int));
+
+    std::cout << "grid_cell_index:" << std::endl;
+    for (int j : grid_cell_index) {
+      std::cout << j << ' ';
+    }
+    std::cout << std::endl;
+
+    if (err != CL_SUCCESS) {
+      std::string error_m = "An ERROR is appearing during work of kernel "
+                            "_ker_grid_cell_indexing; error code is: ";
+      error_m.append(std::to_string(err));
+      throw ocl_error(error_m);
+    }
+    return err;
   }
 };
 } // namespace solver
